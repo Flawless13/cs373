cc3e891c6d59a4dea74f6343642243b3e38c24f4
Glenn Downing <downing@cs.utexas.edu>
2013-10-28 14:05:12 -0500

    another commit

---
+++ quizzes/Quiz23.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+
+"""
+CS373: Quiz #23 (5 pts)
+"""
+
+""" ----------------------------------------------------------------------
+ 1. What's the name of the company that spoke on Fri?
+    (1 pt)
+
+Scopely
+"""
+
+""" ----------------------------------------------------------------------
+ 2. What's their main focus?
+    (1 pt)
+
+gaming platform for iOS and Android
+"""
+
+""" ----------------------------------------------------------------------
+ 3. What's the name of their Unity unit test framework
+    (2 pts)
+
+UnTested
+"""
