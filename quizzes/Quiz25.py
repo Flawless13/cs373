5f2b6ad6fc1c4a9ab089a7cc4769bfd4ae70ae9c
Glenn Downing <downing@cs.utexas.edu>
2013-11-01 19:55:05 -0500

    another commit

---
+++ quizzes/Quiz25.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+"""
+CS373: Quiz #25 (5 pts)
+"""
+
+""" ----------------------------------------------------------------------
+ 1. What is the multiplicity between Rental and Customer?
+    [RF: Ch. 1, pg. 2]
+    (2 pts)
+
+many to one
+"""
+
+""" ----------------------------------------------------------------------
+ 2. What needs to be built, as the first step in refactoring?
+    [RF: Ch. 1, pg. 7]
+    (2 pts)
+
+a set of tests
+"""
